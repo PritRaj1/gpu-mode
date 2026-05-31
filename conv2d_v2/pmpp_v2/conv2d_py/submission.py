10c8a914c5c221731f7e020b7067afd9b3658909
PritRaj1 <prithvi.raj1@outlook.com>
2026-05-30 19:47:30 -0700

    wip

--- conv2d_v2/pmpp_v2/conv2d_py/submission.py
+++ conv2d_v2/pmpp_v2/conv2d_py/submission.py
@@ -3,8 +3,21 @@
 
 from task import input_t, output_t
 import torch
-import torch.nn.functional as F
+# import torch.nn.functional as F
+import pathlib
+from torch.utils.cpp_extension import load_inline
 
+cuda_path = pathlib.Path(__file__).parent / "kernel.cu"
+cuda_source = cuda_path.read_text()
+
+cuda_module = load_inline(
+    name="conv2d_extension",
+    cpp_sources="",
+    cuda_sources=cuda_source,
+    functions=["forward"],
+    with_cuda=True,
+    extra_cflags=["-O3"]
+)
 
 def custom_kernel(data: input_t) -> output_t:
     """
@@ -16,5 +29,14 @@ def custom_kernel(data: input_t) -> output_t:
         Output tensor after convolution
     """
     input_tensor, kernel, output = data
-    output[...] = F.conv2d(input_tensor, kernel, stride=1, padding=0)
+    # output[...] = F.conv2d(input_tensor, kernel, stride=1, padding=0)
+ 
+    if not input_tensor.is_contiguous():
+        input_tensor = input_tensor.contiguous()
+    if not kernel.is_contiguous():
+        kernel = kernel.contiguous()
+    if not output.is_contiguous():
+        output = output.contiguous()
+
+    cuda_module.forward(input_tensor, kernel, output)
     return output
