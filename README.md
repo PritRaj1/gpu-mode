570c1a7e0e7a98216e60c6254d98d98aaad10861
PritRaj1 <prithvi.raj1@outlook.com>
2026-06-07 20:15:07 -0700

    wip: profiling

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Submission for [GPU MODE](https://www.gpumode.com) practice problems.
 ## Links
 
 - [Matmul step-through](https://siboehm.com/articles/22/CUDA-MMM)
+- [Nsight profiling](https://arikpoz.github.io/posts/2025-05-25-speed-up-pytorch-training-by-3x-with-nvidia-nsight-and-pytorch-2-tricks/)
 - [Discord invite](https://discord.com/invite/gpumode)
 - [GPU Glossary](https://modal.com/gpu-glossary)
---
+++ problems/matmul_v2/profile_mm.py
@@ -0,0 +1,33 @@
+import os
+import torch
+from torch.utils.cpp_extension import load
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+device = "cuda"
+
+matmul_cuda = load(
+    name="matmul_cuda_profile",
+    sources=[os.path.join(HERE, "kernel.cu")],
+    extra_cuda_cflags=["-O3", "--ptxas-options=-v"], # verbose
+    verbose=True,
+)
+
+if __name__ == "__main__":
+    M, K, N = 256, 256, 256
+    torch.manual_seed(0)
+    A = torch.randn(M, K, device=device, dtype=torch.float16).contiguous()
+    B = torch.randn(K, N, device=device, dtype=torch.float16).contiguous()
+    C = torch.zeros(M, N, device=device, dtype=torch.float16).contiguous()
+
+    print(f"Profiling mamtul_v2 for shape ({M}, {K}, {N})...")
+
+    # Warnmup to force JIT + ramp-up
+    for _ in range(10):
+        _ = matmul_cuda.forward(A, B, C)
+    torch.cuda.synchronize()
+
+    with torch.cuda.nvtx.range("v1_coalesced_flat_mm"):
+        C_cuda = matmul_cuda.forward(A, B, C)
+        torch.cuda.synchronize()
+
+    print("Profile complete.")
